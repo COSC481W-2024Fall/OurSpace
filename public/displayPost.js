504f3777c7593a19a203a67984bbf53d7169e9d1
ming murray <mmurra33@emich.edu>
2024-10-23 13:34:11 -0400

    added firebase configurations

--- public/displayPost.js
+++ public/displayPost.js
@@ -1,4 +1,18 @@
 function loadPosts() {
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyCppKyFDiC6qSWoS25mP4f-7DUfJ05BWl8",
+        authDomain: "ourspace-9703c.firebaseapp.com",
+        projectId: "ourspace-9703c",
+        storageBucket: "ourspace-9703c.appspot.com",
+        messagingSenderId: "829335148222",
+        appId: "1:829335148222:web:94ebc43b2d3ea171e1b0ef",
+        measurementId: "G-T3YWVB2JGV"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    const auth = firebase.auth();
     const db = firebase.firestore();
     const postsContainer = document.getElementById('postsContainer');  // This is where posts will be rendered
     
--- public/firebasePost.js
+++ public/firebasePost.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const firebaseConfig = {
+        apiKey: "AIzaSyCppKyFDiC6qSWoS25mP4f-7DUfJ05BWl8",
+        authDomain: "ourspace-9703c.firebaseapp.com",
+        projectId: "ourspace-9703c",
+        storageBucket: "ourspace-9703c.appspot.com",
+        messagingSenderId: "829335148222",
+        appId: "1:829335148222:web:94ebc43b2d3ea171e1b0ef",
+        measurementId: "G-T3YWVB2JGV"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    const auth = firebase.auth();
+    
     const db = firebase.firestore();
 
     const postForm = document.getElementById('postForm');
