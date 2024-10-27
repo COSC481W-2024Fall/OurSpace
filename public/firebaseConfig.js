a667ffebc1acc91ff336385ed7e920c9847d76f4
CarolinaMancilla-381 <cmancill@emich.edu>
2024-10-27 17:34:40 -0400

    Completed User Profile and Fixes:
    I have completed the user profile. The user is able to
    successfully see their username on screen and their bio
    if they have one. The user is also able to see their
    profile color. All the buttons are wokring besides two
    (post and add friend). When the user clicks edit, they
    are able to customize their profile and be able to
    save their changes.
    
    I also fixed the homepage so that some of the buttons work.
    To add on, I made a file (one of the peer review suggestions)
    to configure firebase. That way I will not have to
    keep on copying and pasting a whole chunck of code.
    
    Issues:
    1. Need to add user posts
    2. Add the remaining buttons

---
+++ public/firebaseConfig.js
@@ -0,0 +1,14 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCppKyFDiC6qSWoS25mP4f-7DUfJ05BWl8",
+    authDomain: "ourspace-9703c.firebaseapp.com",
+    projectId: "ourspace-9703c",
+    storageBucket: "ourspace-9703c.appspot.com",
+    messagingSenderId: "829335148222",
+    appId: "1:829335148222:web:94ebc43b2d3ea171e1b0ef",
+    measurementId: "G-T3YWVB2JGV"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
